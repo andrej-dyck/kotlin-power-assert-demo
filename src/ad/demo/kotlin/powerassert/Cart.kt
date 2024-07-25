3bf171a653a58c06972bdd77bdb9f863934725e0
Andrej Dyck <43913051+andrej-dyck@users.noreply.github.com>
2024-07-25 19:41:44 +0200

    improve toStrings() for better output

--- src/ad/demo/kotlin/powerassert/Cart.kt
+++ src/ad/demo/kotlin/powerassert/Cart.kt
@@ -6,18 +6,23 @@ data class Cart(val cartId: ShortId, val items: List<CartItem>) {
 
     fun withItem(sku: Sku, quantity: Quantity? = null) =
         copy(items = items + CartItem(itemId = ShortId(), sku, quantity ?: Quantity(1)))
+
+    override fun toString() = "Cart($cartId, $items)"
 }
 
 fun emptyCart() = Cart(cartId = ShortId(), items = emptyList())
 
-data class CartItem(val itemId: ShortId, val sku: Sku, val quantity: Quantity)
+data class CartItem(val itemId: ShortId, val sku: Sku, val quantity: Quantity) {
+
+    override fun toString() = "Item($itemId, $sku, $quantity)"
+}
 
 @JvmInline
 value class Sku(private val asString: String) {
     init {
         require(asString.isNotBlank())
     }
-    override fun toString() = asString
+    override fun toString() = "Sku('$asString')"
 }
 
 @JvmInline
@@ -25,12 +30,12 @@ value class Quantity(private val amount: Int) {
     init {
         require(amount > 0)
     }
-    override fun toString() = amount.toString()
+    override fun toString() = "Quantity($amount)"
 }
 
 @JvmInline
 value class ShortId(private val id: String) {
     constructor() : this(UUID.randomUUID().toString().take(8))
 
-    override fun toString() = id
+    override fun toString() = "Id('$id')"
 }
